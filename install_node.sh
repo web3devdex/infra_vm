da85291dc163e3a10017b651599a672eeaf11e11
web3devdex <34705561+web3devdex@users.noreply.github.com>
2024-09-05 15:17:08 +0700

    Create install_node.sh

---
+++ install_node.sh
@@ -0,0 +1,6 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install v20.9.0
+npm i -g yarn pm2 next express-generator
+pm2 install pm2-server-monit
